c18c0f2339bd3909e511fcc14c4447be3b40c095
unknown <sadatsayedibrahim@gmail.com>
2020-07-03 11:12:47 -0700

    nine

---
+++ src/main/java/us/tekschool/testNG/Ninth/Practice9/App.java
@@ -0,0 +1,13 @@
+package us.tekschool.testNG.Ninth.Practice9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
